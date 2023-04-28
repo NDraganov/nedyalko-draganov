b505b1d70fca6ac6714ac409bad6bec3e79c33f3
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-04-29 00:04:17 +0100

    Add social media links icons and style the footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,25 +1,24 @@
 import Link from "next/link";
 import NavigationBrand from "../navigation/navigation-brand";
+import CurrentYear from "./current-year";
+import { LinkedIn, GitHub } from "@mui/icons-material";
 
 import classes from "./footer.module.css";
-import CurrentYear from "./current-year";
 
 const Footer = () => {
+  const linkedHref = "https://www.linkedin.com/in/nedyalko-draganov-12958a235/";
+  const gitHref = "https://github.com/NDraganov";
   return (
     <footer className={classes.footer}>
-      <div>
-        <Link
-          href="https://www.linkedin.com/in/nedyalko-draganov-12958a235/"
-          title="LinkedIn"
-          target="_blank"
-        ></Link>
-        <Link
-          href="https://github.com/NDraganov"
-          title="GitHub"
-          target="_blank"
-        ></Link>
+      <div className={classes.section}>
+        <Link href={linkedHref} title="LinkedIn" target="_blank">
+          <LinkedIn sx={{ fontSize: 59 }} />
+        </Link>
+        <Link href={gitHref} title="GitHub" target="_blank">
+          <GitHub sx={{ fontSize: 50 }} />
+        </Link>
       </div>
-      <div>
+      <div className={classes.section}>
         <NavigationBrand />
         <CurrentYear />
       </div>
