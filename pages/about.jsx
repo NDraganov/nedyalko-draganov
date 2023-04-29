69929b2cde9fe1e7cccfd89f71aadcd7340a22e6
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-04-29 09:34:24 +0100

    Create about page and add globally styles for heading

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import classes from "./about.module.css";
+
+const About = () => {
+  return (
+    <div className={classes.about}>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
