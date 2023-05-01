fac97421e6f62f0380e180bed988bc0bf95c1041
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-01 13:40:49 +0100

    Change styles for home links to align them on left for mobile

--- components/home-page/home-links.jsx
+++ components/home-page/home-links.jsx
@@ -8,12 +8,16 @@ const HomeLinks = () => {
   return (
     <Fragment>
       <div className={classes.buttons}>
-        <Link href="/portfolio" title="See my work">
-          <Code sx={{ fontSize: 30 }} /> See my work
-        </Link>
-        <Link href="mailto:nedyalkodraganov90@gmail.com" title="Get in touch">
-          <MailOutline sx={{ fontSize: 30 }} /> Get in touch
-        </Link>
+        <div>
+          <Link href="/portfolio" title="See my work">
+            <Code sx={{ fontSize: 30 }} /> See my work
+          </Link>
+        </div>
+        <div>
+          <Link href="mailto:nedyalkodraganov90@gmail.com" title="Get in touch">
+            <MailOutline sx={{ fontSize: 30 }} /> Get in touch
+          </Link>
+        </div>
       </div>
     </Fragment>
   );
