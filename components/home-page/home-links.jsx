e67cfb32663b3ea8ae377e372616d0ffe3157327
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-03 00:08:08 +0100

    Add individually timer for buttons animation

--- components/home-page/home-links.jsx
+++ components/home-page/home-links.jsx
@@ -12,17 +12,17 @@ const HomeLinks = () => {
   return (
     <Fragment>
       <div className={classes.buttons}>
-        <div>
+        <div className={classes.first}>
           <Link href="/portfolio" title="See my work">
             <Code sx={{ fontSize: 30 }} /> See my work
           </Link>
         </div>
-        <div>
+        <div className={classes.second}>
           <Link href="mailto:nedyalkodraganov90@gmail.com" title="Get in touch">
             <MailOutline sx={{ fontSize: 30 }} /> Get in touch
           </Link>
         </div>
-        <div>
+        <div className={classes.third}>
           <Link href="/images/resume.pdf" target="_blank" title="Resume">
             <InsertDriveFileOutlined sx={{ fontSize: 26 }} /> Resume
           </Link>
